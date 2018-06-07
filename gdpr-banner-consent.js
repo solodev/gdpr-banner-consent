ff8b893e94a36551f4c13a7ec2cc85c4d7f4c355
Scott Madara <smmadara@gmail.com>
2018-06-07 16:59:24 -0400

    Update gdpr-banner-consent.js

--- gdpr-banner-consent.js
+++ gdpr-banner-consent.js
@@ -1,16 +1,16 @@
 // Banner Trigger if Not Closed
-if (!sessionStorage.bannerClosed) {
+if (!localStorage.bannerClosed) {
   $('.privacy-banner').css('display', 'inherit');
 } else {
   $('.privacy-banner').css('display', 'none');
 }
 $('.privacy-banner button').click(function() {
   $('.privacy-banner').css('display', 'none');
-  sessionStorage.bannerClosed = 'true';
+  localStorage.bannerClosed = 'true';
 });
 $('.banner-accept').click(function() {
   $('.privacy-banner').css('display', 'none');
-  sessionStorage.bannerClosed = 'true';
+  localStorage.bannerClosed = 'true';
 });
 if (navigator.userAgent.match(/Opera|OPR\//)) {
   $('.privacy-banner').css('display', 'inherit');
